08e432fde910e5a902f51ce52c303a6b5ad8384d
chayadeaver <chayadeaver@gmail.com>
2021-01-08 13:27:15 -0500

    create itemUpdateSubmit in App

--- app.js
+++ app.js
@@ -153,6 +153,9 @@ const App = (function(ItemCrl, UICtrl){
 
     // Add item event
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
+
+    // Edit icon click event
+    document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
   }
 
   // Add item submit
@@ -174,6 +177,25 @@ const App = (function(ItemCrl, UICtrl){
 
       // Clear fields
       UICtrl.clearInput();
+    }
+
+    e.preventDefault();
+  }
+
+  // Update item submit
+  const itemUpdateSubmit = function(e){
+    if(e.target.classList.contains('edit-item')){
+      // Get list item id (item-0, item-1)
+      const listId = e.target.parentNode.parentNode.id;
+      
+      // Break into an array
+      const listIdArr = listId.split('-');
+      
+      // Get the actual id
+      const id = parseInt(listIdArr[1]);
+
+      // Get item
+      const itemToEdit = ItemCtrl.getItemById(id);
     }
 
     e.preventDefault();
