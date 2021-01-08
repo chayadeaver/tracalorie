ce188e5d526353b156f9fcfcad6362729798bdb8
chayadeaver <chayadeaver@gmail.com>
2021-01-08 13:01:36 -0500

    show total calories added with each meal

--- app.js
+++ app.js
@@ -47,6 +47,20 @@ const ItemCtrl = (function(){
 
       return newItem;
     },
+    getTotalCalories: function(){
+      let total = 0;
+
+      // Loop through items and add cals
+      data.items.forEach(function(item){
+        total += item.calories;
+      });
+
+      // Set total cal in data structure
+      data.totalCalories = total;
+
+      // Return total
+      return data.totalCalories;
+    },
     logData: function(){
       return data;
     }
@@ -60,7 +74,8 @@ const UICtrl = (function(){
     itemList: '#item-list',
     addBtn: '.add-btn',
     itemNameInput: '#item-name',
-    itemCaloriesInput: '#item-calories'
+    itemCaloriesInput: '#item-calories',
+    totalCalories: '.total-calories'
   }
 
   // Public methods
@@ -109,6 +124,9 @@ const UICtrl = (function(){
     hideList: function(){
       document.querySelector(UISelectors.itemList).style.display = 'none';
     },
+    showTotalCalories: function(totalCalories){
+      document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+    },  
     getSelectors: function(){
       return UISelectors;
     }
@@ -139,7 +157,13 @@ const App = (function(ItemCrl, UICtrl){
       // Add item to UI list
       UICtrl.addListItem(newItem);
 
-      // Clear input
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+
+      // Clear fields
       UICtrl.clearInput();
     }
 
