31b1816e279aee360ff7daf3066aac25289759a1
chayadeaver <chayadeaver@gmail.com>
2021-01-08 13:49:00 -0500

    rename function to itemEditClick

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ const App = (function(ItemCrl, UICtrl){
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
 
     // Edit icon click event
-    document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+    document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
   }
 
   // Add item submit
@@ -209,8 +209,8 @@ const App = (function(ItemCrl, UICtrl){
     e.preventDefault();
   }
 
-  // Update item submit
-  const itemUpdateSubmit = function(e){
+  // Click edit item
+  const itemEditClick = function(e){
     if(e.target.classList.contains('edit-item')){
       // Get list item id (item-0, item-1)
       const listId = e.target.parentNode.parentNode.id;
